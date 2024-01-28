11fba615b4cf29fff48afff2c9f798d878d5f1cf
impROS <impROS@hotmail.com>
2024-01-29 01:55:49 +0300

    sync

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import org.springframework.javapoet.JavaFile;
+import org.springframework.javapoet.MethodSpec;
+import org.springframework.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // MethodSpec oluşturma
+        MethodSpec myMethod = MethodSpec.methodBuilder("myMethod")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addStatement("System.out.println(\"Hello, World!\")")
+                .build();
+
+        // TypeSpec (sınıf) oluşturma ve yukarıda oluşturduğumuz metodu ekme
+        TypeSpec myClass = TypeSpec.classBuilder("MyClass")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(myMethod)
+                .build();
+
+
+        // Java dosyasını oluşturma
+        JavaFile javaFile = JavaFile.builder("com.example", myClass)
+                .build();
+
+        // Oluşturulan Java dosyasını konsola yazdırma
+        try {
+            javaFile.writeTo(System.out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
